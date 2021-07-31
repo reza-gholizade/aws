f4ac9f20c2943a58d0aef01d374264fa1584ad31
reza.gholizadeh <reza.gholizadeh@snapptrip.com>
2021-08-01 02:30:14 +0430

    fix: edit aws_ecs_task_definition resource

--- ecs.tf
+++ ecs.tf
@@ -24,34 +24,21 @@ data "template_file" "env_vars" {
 resource "aws_ecs_task_definition" "aws-ecs-task" {
   family = "${var.app_name}-task"
 
-  container_definitions = <<DEFINITION
-  [
+  container_definitions = jsonencode([
     {
-      "name": "${var.app_name}-${var.app_environment}-container",
-      "image": "${aws_ecr_repository.metabase.repository_url}:v1.0.0",
-      "entryPoint": [],
-      "environment": ${data.template_file.env_vars.rendered},
-      "essential": true,
-      "logConfiguration": {
-        "logDriver": "awslogs",
-        "options": {
-          "awslogs-group": "${aws_cloudwatch_log_group.log-group.id}",
-          "awslogs-region": "${var.aws_region}",
-          "awslogs-stream-prefix": "${var.app_name}-${var.app_environment}"
-        }
-      },
-      "portMappings": [
+      name      = "${var.app_name}-${var.app_environment}-container"
+      image     = "${aws_ecr_repository.metabase.repository_url}:v1.0.0"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
         {
-          "containerPort": 8080,
-          "hostPort": 8080
+          containerPort = 8080
+          hostPort      = 8080
         }
-      ],
-      "cpu": 256,
-      "memory": 512,
-      "networkMode": "awsvpc"
+      ]
     }
-  ]
-  DEFINITION
+  ])
 
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
--- terraform.tfvars
+++ terraform.tfvars
@@ -7,6 +7,8 @@ availability_zones = ["eu-central-1a", "eu-central-1b"]
 public_subnets     = ["10.10.100.0/24", "10.10.101.0/24"]
 private_subnets    = ["10.10.0.0/24", "10.10.1.0/24"]
 
+cluster_name    = "fleetspark-production-cluster"
+image_name      = "501146222760.dkr.ecr.eu-central-1.amazonaws.com/metabase:v1.0.0"
 app_name        = "app"
 app_environment = "production"
 
