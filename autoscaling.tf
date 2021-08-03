27767d02d08fa5bbdb2fdcb44c09acd8a6e4f97b
Reza Gholizade <gholizade.net@gmail.com>
2021-08-04 01:14:10 +0430

    change: increase max_capacity

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,7 +1,7 @@
 # autoscaling.tf | Auto Scaling Group
 
 resource "aws_appautoscaling_target" "ecs_target" {
-  max_capacity       = 2
+  max_capacity       = 3
   min_capacity       = 1
   resource_id        = "service/${aws_ecs_cluster.aws-ecs-cluster.name}/${aws_ecs_service.aws-ecs-service.name}"
   scalable_dimension = "ecs:service:DesiredCount"
